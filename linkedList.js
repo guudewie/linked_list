713207861344478cb9c175dd4789f8d743e9e1ae
guudewie <philippthiessen12@gmail.com>
2023-09-27 18:55:30 +0200

    set up files and write Linked List Factory

---
+++ linkedList.js
@@ -0,0 +1,65 @@
+
+const LinkedList = () => {
+
+    const list = { head: null}
+
+    const append = (value) => {
+
+        if (list.head === null) {
+            list.head = { value : value, next : null}
+            return
+        }
+
+        let currentNode = list.head;
+
+        while (currentNode.next !== null) {
+            currentNode = currentNode.next
+        }
+        currentNode.next = { value : value, next : null}
+    }
+ 
+    const prepend = (value) => {
+
+        if (list.head === null) {
+            list.head = { value : value, next : null}
+            return
+        }
+
+        list.head = { value : value, next : list.head}
+    }
+
+
+    return {
+        list,
+        append,
+        prepend
+    }
+}
+
+const firstList = LinkedList();
+firstList.append(10);
+firstList.append(20);
+firstList.prepend(30)
+
+
+console.log(firstList.list)
+
+
+
+
+
+const Node = (value = null, nextNode = null) => {
+
+    const getValue = () => value;
+    const setValue = (newValue) => value = newValue;
+
+    const getNextNode = () => nextNode;
+    const setNextNode = (newNextNode) => nextNode = newNextNode;
+
+    return {
+        getValue,
+        setValue,
+        getNextNode,
+        setNextNode
+    }
+}
